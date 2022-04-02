efcdcc7138b7f0a222b4cfabeea1a6c7aa2bd6b9
bhchoi84 <bhchoi84@gmail.com>
2022-04-02 15:49:18 +0900

    소스 기본틀 첫커밋

---
+++ src/main/java/bitecoder/com/todo/ReactTodoServerApplication.java
@@ -0,0 +1,13 @@
+package bitecoder.com.todo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactTodoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactTodoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bitecoder/com/todo/ReactTodoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package bitecoder.com.todo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactTodoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
